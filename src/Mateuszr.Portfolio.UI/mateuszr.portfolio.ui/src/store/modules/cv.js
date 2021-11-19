9380be66fa62d6070e10029897b2ee24a7151a46
Mateusz Rusek <rusek.mateusz@poczta.pl>
2021-11-19 20:10:45 +0100

    -further work on stores

--- src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/store/modules/cv.js
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/store/modules/cv.js
@@ -1,14 +1,21 @@
+import cvService from '../../api/cvService';
+
 const state = () => ({
-   
+    jobDetails: []
 })
 const mutations = {
-  
+    setJobDetails(state, jobDetails) {
+        state.jobDetails = [...jobDetails];
+    }
 }
 const actions = {
-   
+    async fetchJobDetails({ commit }) {
+        const jobDetails = await cvService.getJobDetails();
+        commit('setJobDetails', jobDetails);
+    }
 }
 const getters = {
-   
+
 }
 
 export default function createCvStore() {
--- src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/store/modules/skills.js
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/store/modules/skills.js
@@ -1,3 +1,4 @@
+import skillsService from '../../api/skillsService';
 
 const state = () => ({
     hardSkills: [],
@@ -5,10 +6,29 @@ const state = () => ({
     passions: []
 })
 const mutations = {
-   
+    setHardSkills(state, hardSkills) {
+        state.hardSkills = [...hardSkills];
+    },
+    setSoftSkills(state, softSkills) {
+        state.softSkills = [...softSkills];
+    },
+    setPassions(state, passions) {
+        state.passions = [...passions];
+    }
 }
 const actions = {
-  
+    async fetchHardSkills({ commit }) {
+        const hardSkills = await skillsService.getHardSkills();
+        commit('setHardSkills', hardSkills);
+    },
+    async fetchSoftSkills({ commit }) {
+        const softSkills = await skillsService.getSoftSkills();
+        commit('setSoftSkills', softSkills);
+    },
+    async fetchPassions({ commit }) {
+        const passions = await skillsService.getPassions();
+        commit('setPassions', passions);
+    }
 }
 const getters = {
 
