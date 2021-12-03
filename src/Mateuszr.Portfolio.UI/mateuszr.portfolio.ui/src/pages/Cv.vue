da3f8aabe9c2ba882768e64ef50008882313a17f
Mateusz Rusek <rusek.mateusz@poczta.pl>
2021-12-03 21:53:18 +0100

    -mounted CV in a proper page -overlayed cv with pointers that will display further info

--- src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/pages/Cv.vue
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/pages/Cv.vue
@@ -2,8 +2,19 @@
   <q-page>
     <div class="page-container">
       <div class="fit-content">
-      <img src="https://arxiv.org/pdf/1407.5527.pdf" />
-      <div>CV</div>
+      <img src="../assets/CV_PL.jpg" />
+      <div class="concentric-circles-1-container">
+        <div class="concentric-circles"></div>
+      </div>
+      <div class="concentric-circles-2-container">
+        <div class="concentric-circles"></div>
+      </div>
+      <div class="concentric-circles-3-container">
+        <div class="concentric-circles"></div>
+      </div>
+      <div class="concentric-circles-4-container">
+        <div class="concentric-circles"></div>
+      </div>
       </div>
     </div>
   </q-page>
