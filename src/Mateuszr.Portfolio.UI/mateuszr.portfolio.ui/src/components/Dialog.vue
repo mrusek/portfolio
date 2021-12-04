b17335739ec15fef5c32f25b7ff48b348f1a9e92
Mateusz Rusek <rusek.mateusz@poczta.pl>
2021-12-04 14:50:05 +0100

    -Extracted Dialog to separate component

---
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/components/Dialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-dialog v-model="isVisible" @show="onOpen" @hide="onClose">
+    <q-card>
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6">{{ title }}</div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <q-card-section>
+        {{ content }}
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+
+<script>
+export default {
+  name: "Dialog",
+  props: ["visible", "title", "content", "onClose", "onOpen"],
+  model: {
+    prop: "visible",
+    event: "visibleChange",
+  },
+  computed: {
+    isVisible: {
+      get: function () {
+        return this.visible;
+      },
+      set: function (value) {
+        this.$emit("visibleChange", value);
+      },
+    },
+  },
+  setup() {},
+  data() {
+    return {};
+  },
+};
+</script>
--- src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/pages/Cv.vue
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/pages/Cv.vue
@@ -2,31 +2,36 @@
   <q-page>
     <div class="page-container">
       <div class="fit-content">
-      <img src="../assets/CV_PL.jpg" />
-      <div class="concentric-circles-1-container">
-        <div class="concentric-circles"></div>
-      </div>
-      <div class="concentric-circles-2-container">
-        <div class="concentric-circles"></div>
-      </div>
-      <div class="concentric-circles-3-container">
-        <div class="concentric-circles"></div>
-      </div>
-      <div class="concentric-circles-4-container">
-        <div class="concentric-circles"></div>
-      </div>
+        <img src="../assets/CV_PL.jpg" />
+        <div class="concentric-circles-1-container">
+          <div class="concentric-circles" @click="isFirstVisible = true"></div>
+        </div>
+        <Dialog content="Transition technologies" v-model="isFirstVisible" title="Staż w Transition Technologies"></Dialog>
+        <div class="concentric-circles-2-container">
+          <div class="concentric-circles"></div>
+        </div>
+        <div class="concentric-circles-3-container">
+          <div class="concentric-circles"></div>
+        </div>
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
+import { ref } from "vue";
+import Dialog from "../components/Dialog.vue";
 export default {
   name: "Cv",
   components: {
+    Dialog,
   },
   data() {
+    return {};
+  },
+  setup() {
     return {
+      isFirstVisible: ref(false),
     };
   },
 };
