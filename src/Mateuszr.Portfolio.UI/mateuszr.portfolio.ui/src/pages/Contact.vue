702d48e07bcd356d0b9b0d1bd99fe945594c74f6
Mateusz Rusek <rusek.mateusz@poczta.pl>
2022-01-21 21:56:33 +0100

    -finished contact page

--- src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/pages/Contact.vue
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/pages/Contact.vue
@@ -3,9 +3,9 @@
     <div class="card-container">
       <div class="card">
         <div class="card-back">
-        <div><a href="https://github.com/mrusek">github.com/mrusek</a></div>
-        <div><a href="https://www.linkedin.com/in/mateusz-rusek-b3b275113">/in/mateusz-rusek</a></div>
-        <div><a href="https://t.me/mrusek">t.me/mrusek</a></div>
+        <div class="card-link shadow-4"><a href="https://github.com/mrusek" target="_blank">github.com/mrusek</a></div>
+        <div class="card-link shadow-4"><a href="https://www.linkedin.com/in/mateusz-rusek-b3b275113" target="_blank">/in/mateusz-rusek</a></div>
+        <div class="card-link shadow-4"><a href="https://t.me/mrusek" target="_blank">t.me/mrusek</a></div>
         </div>
         <div class="card-front">
           <img :src="github" class="icon" />
