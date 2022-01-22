a9fc690eb7086f852807dceab2967f4e46b60cab
Mateusz Rusek <rusek.mateusz@poczta.pl>
2022-01-22 20:29:40 +0100

    -added landing page -little tweaks for style

--- src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/App.vue
+++ src/Mateuszr.Portfolio.UI/mateuszr.portfolio.ui/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="q-pa-md">
-    <q-layout view="lHh Lpr lff" class="shadow-2 rounded-borders">
+  <div class="q-pa-md bg-main">
+    <q-layout view="lHh Lpr lff">
       <q-header class="bg-teal shadow-2">
         <q-toolbar class="glossy">
           <!-- <q-btn flat @click="drawer = !drawer" round dense icon="menu" /> -->
@@ -9,7 +9,7 @@
       </q-header>
 
       <q-drawer
-        elevated
+        class="shadow-12"
         v-model="drawer"
         show-if-above
         :mini="miniState"
@@ -17,43 +17,73 @@
         @mouseout="miniState = true"
         :width="200"
         :breakpoint="500"
-        bordered
-        class="bg-grey-3"
       >
-        <q-scroll-area style="height: calc(100% - 100px); margin-top: 50px; border-right: 1px solid #ddd">
-          <q-list >
-            <q-item clickable v-ripple @click="$router.push({ path: '/cv', replace: true })">
+        <q-scroll-area style="height: calc(100% - 100px); margin-top: 50px">
+          <q-list>
+            <q-item
+              clickable
+              v-ripple
+              @click="$router.push({ path: '/cv', replace: true })"
+            >
               <q-item-section avatar>
                 <q-icon name="far fa-list-alt" class="text-teal" />
               </q-item-section>
 
-              <q-item-section> CV </q-item-section>
+              <q-item-section
+                ><span style="color: whitesmoke; font-size: 21px">
+                  CV
+                </span></q-item-section
+              >
             </q-item>
-
-            <q-item clickable v-ripple @click="$router.push({ path: '/description', replace: true })">
+            <q-separator size="2px" :dark="true" />
+            <q-item
+              clickable
+              v-ripple
+              @click="$router.push({ path: '/description', replace: true })"
+            >
               <q-item-section avatar>
                 <q-icon name="fas fa-edit" class="text-teal" />
               </q-item-section>
 
-              <q-item-section> Description </q-item-section>
+              <q-item-section
+                ><span style="color: whitesmoke; font-size: 21px">
+                  Description
+                </span></q-item-section
+              >
             </q-item>
-
-            <q-item clickable v-ripple  @click="$router.push({ path: '/skills', replace: true })">
+            <q-separator size="2px" :dark="true" />
+            <q-item
+              clickable
+              v-ripple
+              @click="$router.push({ path: '/skills', replace: true })"
+            >
               <q-item-section avatar>
                 <q-icon name="fas fa-hat-wizard" class="text-teal" />
               </q-item-section>
 
-              <q-item-section> Skills </q-item-section>
+              <q-item-section
+                ><span style="color: whitesmoke; font-size: 21px">
+                  Skills
+                </span></q-item-section
+              >
             </q-item>
 
-            <q-separator />
+            <q-separator size="2px" :dark="true" />
 
-            <q-item clickable v-ripple  @click="$router.push({ path: '/contact', replace: true })">
+            <q-item
+              clickable
+              v-ripple
+              @click="$router.push({ path: '/contact', replace: true })"
+            >
               <q-item-section avatar>
                 <q-icon name="fas fa-address-card" class="text-teal" />
               </q-item-section>
 
-              <q-item-section> Contact </q-item-section>
+              <q-item-section
+                ><span style="color: whitesmoke; font-size: 21px">
+                  Contact
+                </span></q-item-section
+              >
             </q-item>
           </q-list>
         </q-scroll-area>
@@ -62,7 +92,10 @@
           src="https://cdn.quasar.dev/img/material.png"
           style="height: 50px"
         >
-          <div class="absolute-bottom bg-transparent padding-3 link" @click="$router.push({ path: '/', replace: true })">
+          <div
+            class="absolute-bottom bg-transparent padding-3 link"
+            @click="$router.push({ path: '/', replace: true })"
+          >
             <q-avatar size="46px" class="q-mb-sm margin-bottom-0">
               <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
@@ -72,7 +105,7 @@
       <q-page-container>
         <router-view></router-view>
       </q-page-container>
-      <q-footer style="background-color: white; color: black">
+      <q-footer style="background: none">
         <span>© Mateusz Rusek 2021</span></q-footer
       >
     </q-layout>
@@ -82,7 +115,7 @@
 <script>
 import { ref } from "vue";
 //import HelloWorld from './components/HelloWorld.vue'
-import router from 'vue-router';
+import router from "vue-router";
 export default {
   name: "App",
 
